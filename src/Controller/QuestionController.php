c675613e4f96ce8fb063a5474451f4813bc92e09
Ante Filipović <Ante.filipovic72@gmail.com>
2021-07-12 00:30:09 +0200

    Hello world

---
+++ src/Controller/QuestionController.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+
+class QuestionController
+{
+
+    public function homepage()
+    {
+        
+       return new Response('Hello world'); 
+
+    }
+
+}
+
+
+?>
