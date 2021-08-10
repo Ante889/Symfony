7d811d84ac22456202164420cfc044696a42afb1
Ante Filipović <Ante.filipovic72@gmail.com>
2021-08-10 13:24:34 +0200

    Adding Search function

--- SummerProject-Symfony/src/Controller/HomeController.php
+++ SummerProject-Symfony/src/Controller/HomeController.php
@@ -1,10 +1,10 @@
 <?php
 
 namespace App\Controller;
-
 use App\Entity\Products;
 use App\Form\ProductType;
 use App\Repository\ProductsRepository;
+use Doctrine\ORM\Mapping\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,6 +18,19 @@ class HomeController extends AbstractController
         $Products= $pro->findAll();
 
         return $this->render('home/index.html.twig', [
+            'Products' => $Products
+        ]);
+    }
+    #[Route('/search', name: 'search')]
+    public function search(Request $request): Response
+    {
+        $em=$this->getDoctrine()->getManager();
+        $Products= $em -> getRepository(Products::class)->findAll();
+        if($request->isMethod("POST")){
+            $search = $request->get('search');
+            $Products = $em->getRepository(Products::class)->findBy(['title'=>$search]);
+        }
+        return $this->render('home/search.html.twig', [
             'Products' => $Products
         ]);
     }
